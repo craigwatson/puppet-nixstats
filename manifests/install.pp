f14c86fc388ff39872aff8377038d3d771227b36
Craig Watson <craig@cwatson.org>
2016-06-11 18:31:31 +0100

    Using stdlib's ensure_packages

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,43 +19,16 @@
 class nixstats::install {
 
   if $::nixstats::manage_packages == true {
-    if !defined(Package['curl']) {
-      package { 'curl':
-        ensure => present,
-      }
-
-      Exec['download_nixstats'] {
-        require => [File['/etc/nixstats'],Package['curl']],
-      }
-    }
-
-    if !defined(Package['gzip']) {
-      package { 'gzip':
-        ensure => present,
-      }
-    }
-
-    if !defined(Package['sysstat']) {
-      package { 'sysstat':
-        ensure => present,
-      }
-    }
-
-    if !defined(Package[$::nixstats::params::cron_package]) {
-      package { $::nixstats::params::cron_package:
-        ensure => present,
-      }
-    }
+    ensure_packages(['curl','gzip','sysstat',$::nixstats::params::cron_package], {'ensure' => 'present'})
+    $require = [File['/etc/nixstats'],Package['curl']]
   } else {
-    Exec['download_nixstats'] {
-      require => File['/etc/nixstats'],
-    }
+    $require = File['/etc/nixstats']
   }
 
   exec { 'download_nixstats':
     command => '/usr/bin/curl -k -o /etc/nixstats/nixstats.sh https://api.nixstats.com/nixstats.sh',
     creates => '/etc/nixstats/nixstats.sh',
-    require => [File['/etc/nixstats'],Package['curl']],
+    require => $require,
   }
 
 }
